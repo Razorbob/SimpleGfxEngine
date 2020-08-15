5bc5f36265e7faeaaf1bed301f1ba57f93939d7d
Vincent Dibon <vincent.dibon@gmail.com>
2020-08-15 14:44:44 +0200

    Fixed ViewSetup Issues

---
+++ SimpleGfxEngine/MyMetalView.swift
@@ -0,0 +1,31 @@
+//
+//  MyMetalView.swift
+//  SimpleGfxEngine
+//
+//  Created by Vincent Dibon on 15.08.20.
+//
+
+import MetalKit
+
+class MyMetalView: MTKView{
+    
+    var renderer: Renderer!
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        print("Jebemti")
+        
+        // create the GPU Representation
+        self.device = MTLCreateSystemDefaultDevice()
+        
+        //Set Pixel Format to four 8-bit normalized unsigned integer components in Blue Green Red Alpha (BGRA) order.
+        self.colorPixelFormat = .bgra8Unorm
+        
+        //Set the clear Color. Before the Frame gets drawn it is set to its clearColor
+        self.clearColor = MTLClearColor(red: 0.2, green: 0.2, blue: 0.8 , alpha: 1)
+        
+        self.renderer = Renderer(device: self.device!)
+        self.delegate = renderer
+    }
+}
